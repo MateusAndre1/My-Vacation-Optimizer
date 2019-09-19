8d6ef5f879b6737ba2af26486a4c6e3da818048e
Your Name <you@example.com>
2019-09-19 19:13:28 -0400

    edited comments

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -7,14 +7,14 @@ var player = "";
 var done = false;
 var tag = document.createElement('script');
 
-// 2. This code loads the IFrame Player API code asynchronously.
+// This code loads the IFrame Player API code asynchronously.
 
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
 
 function youtubePlay() {
     console.log("In the function", id)
@@ -29,13 +29,13 @@ function youtubePlay() {
     });
 }
 
-// 4. The API will call this function when the video player is ready.
+// The API will call this function when the video player is ready.
 
 function onPlayerReady(event) {
     event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
+// The API calls this function when the player's state changes.
 //    The function indicates that when playing a video (state=1),
 //    the player should play for six seconds and then stop.
 
