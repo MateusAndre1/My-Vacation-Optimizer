98bf40f901972296a72a7b0b087efd7b56691254
Your Name <you@example.com>
2019-09-19 14:14:34 -0400

    video embeder is added on js

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,6 +1,48 @@
-let api_youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10";
+let api_youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1";
 let api_key = "AIzaSyCPJNHeGp_0ek10gfrLP0jcJEEA7yELeZ4";
-let input = "orlando, fl vacation";
+let input = "orlando, fl vacation guide"; //J3SqMNGsptI
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '585',
+    width: '960',
+    videoId: "LfCsOwMc3hk",
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    // setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+
 
 function youtubeCall() {
     let url = api_youtube + "&q=" + input + "&key="+ api_key;
@@ -10,10 +52,8 @@ function youtubeCall() {
         method: "GET"
     })
     .then(function (response){
-        console.log(response);
+        console.log(response.items[0].id.videoId);
     })
-
-
 }
 
 youtubeCall();
