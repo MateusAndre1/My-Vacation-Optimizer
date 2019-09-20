5c6b1fed715e2855d84687b89bbd3d564820d084
Your Name <you@example.com>
2019-09-20 03:04:34 -0400

    added google Places API

--- assets/javascript/youtube.js
+++ assets/javascript/youtube.js
@@ -1,71 +1,101 @@
-// global variables
+$(document).ready(function () {
 
-let api_youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1";
-let api_key = "AIzaSyCPJNHeGp_0ek10gfrLP0jcJEEA7yELeZ4";
-let input = "jamaica vacation guide";
-var player = "";
-var done = false;
-var tag = document.createElement('script');
+    // global variables
 
-// This code loads the IFrame Player API code asynchronously.
+    let api_youtube = "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5";
+    let api_key = "AIzaSyCPJNHeGp_0ek10gfrLP0jcJEEA7yELeZ4";
+    let input = "jamaica vacation tour guide";
+    var player = "";
+    var done = false;
+    var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// This function creates an <iframe> (and YouTube player)
-// after the API code downloads.
-
-function youtubePlay() {
-    console.log("In the function", id)
-    player = new YT.Player('player', {
-        height: '585',
-        width: '960',
-        videoId: id,
-        events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-        }
-    });
-}
-
-// The API will call this function when the video player is ready.
-
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
-
-// The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        done = true;
+    function loadMap() {
+        $(".tourmap").empty();
+        var mapview = ` 
+        <iframe class="info-map" width="960" height="600" frameborder="0" style="border:0"
+        src="https://www.google.com/maps/embed/v1/search?q=${input}&key=AIzaSyCPJNHeGp_0ek10gfrLP0jcJEEA7yELeZ4"
+        allowfullscreen></iframe>
+        `;
+        // console.log(mapview)
+        $(".tourmap").append(mapview);
     }
-}
 
-function stopVideo() {
-    player.stopVideo();
-}
-// call youtube API to retrieve data
+    loadMap();
 
-function youtubeCall() {
-    let url = api_youtube + "&q=" + input + "&key=" + api_key;
 
-    $.ajax({
-            url: url,
-            method: "GET"
-        })
-        .then(function (response) {
-            id = response.items[0].id.videoId;
-            console.log("In promise", id);
-            youtubePlay();
-        })
-}
 
-function youtubeOnClick() {
-    youtubeCall();
-}
+    // This code loads the IFrame Player API code asynchronously.
 
-$("#player").on("click", youtubeOnClick);
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    // This function creates an <iframe> (and YouTube player)
+    // after the API code downloads.
+
+    function youtubeTemplate(itemsid) {
+
+        console.log("In the function", itemsid)
+        player = new YT.Player('player', {
+            height: '585',
+            width: '960',
+            videoId: itemsid,
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+
+    // The API will call this function when the video player is ready.
+
+    function onPlayerReady(event) {
+        event.target.pauseVideo();
+    }
+
+    // The API calls this function when the player's state changes.
+    // The function indicates that when playing a video (state=1),
+    // the player should play for six seconds and then stop.
+
+    function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.PLAYING && !done) {
+            done = true;
+        }
+    }
+
+    function stopVideo() {
+        player.stopVideo();
+    }
+
+    //
+
+    function videoArr(itemsid) {
+        $("#player").empty();
+        const videoTemplate = youtubeTemplate(itemsid);
+        $("#player").append(videoTemplate);
+    }
+
+
+    // call youtube API to retrieve data
+
+    function youtubeCall() {
+        let url = api_youtube + "&q=" + input + "&key=" + api_key;
+
+        $.ajax({
+                url: url,
+                method: "GET"
+            })
+            .then(function (response) {
+                let itemsid = response.items[0].videoId;
+                console.log("In promise", itemsid);
+                videoArr(itemsid);
+            })
+    }
+
+    function youtubeOnClick() {
+        youtubeCall();
+    }
+
+    $("#player").on("click", youtubeOnClick);
+
+});
